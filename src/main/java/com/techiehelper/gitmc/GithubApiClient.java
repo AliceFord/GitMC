4746109a93d7061f379eeddc2dd7e00b2cabcdcd
TechieHelper <oliverlford@icloud.com>
2021-05-22 11:24:23 +0100

    Build protection fix

--- src/main/java/com/techiehelper/gitmc/GithubApiClient.java
+++ src/main/java/com/techiehelper/gitmc/GithubApiClient.java
@@ -1,18 +1,13 @@
 package com.techiehelper.gitmc;
 
-import javafx.util.Pair;
-import org.jetbrains.annotations.NotNull;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class GithubApiClient {
